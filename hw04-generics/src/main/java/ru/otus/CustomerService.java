5329c95be48802cfe23b29e6055ff4e876852c12
 Shendor <apolyakov@archive.systems>
2025-07-11 19:51:33 +0300

    Применение коллекций

--- hw04-generics/src/main/java/ru/otus/CustomerService.java
+++ hw04-generics/src/main/java/ru/otus/CustomerService.java
@@ -1,21 +1,34 @@
 package ru.otus;
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
 
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    public TreeMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    public TreeMap<Customer, String> copyMap = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.firstEntry();
+        Customer original = entry.getKey();
+        Customer copy = new Customer(original.getId(), original.getName(), original.getScores());
+        return Map.entry(copy, entry.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.higherEntry(customer);
+        if (entry == null) {
+            return null;
+        }
+        Customer original = entry.getKey();
+        Customer copy = new Customer(original.getId(), original.getName(), original.getScores());
+        return Map.entry(copy, entry.getValue());
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        Customer copy = new Customer(customer.getId(), customer.getName(), customer.getScores());
+        map.put(customer, data);
+        copyMap.put(copy, data);
+    }
 }

26fc7c81a1d66716d18edb0d0791a8cd3870b7f8
 Shendor <apolyakov@archive.systems>
2025-08-22 11:12:53 +0300

    Подправил по замечаниям

--- hw07-aop/src/main/java/ru/otus/aop/LoggingProxy.java
+++ hw07-aop/src/main/java/ru/otus/aop/LoggingProxy.java
@@ -4,6 +4,10 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public final class LoggingProxy {
     private LoggingProxy() {}
@@ -16,21 +20,41 @@ public final class LoggingProxy {
 
     private static class LogInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface target;
+        private final Map<Method, Method> implByInterface;
+        private final Set<Method> methodsToLog;
 
         LogInvocationHandler(TestLoggingInterface target) {
             this.target = target;
+            this.implByInterface = new HashMap<>();
+            this.methodsToLog = new HashSet<>();
+
+            for (Method ifaceMethod : TestLoggingInterface.class.getMethods()) {
+                try {
+                    Method implMethod =
+                            target.getClass().getMethod(ifaceMethod.getName(), ifaceMethod.getParameterTypes());
+                    implByInterface.put(ifaceMethod, implMethod);
+                    if (implMethod.isAnnotationPresent(Log.class)) {
+                        methodsToLog.add(ifaceMethod);
+                    }
+                } catch (NoSuchMethodException e) {
+                    throw new IllegalStateException(
+                            "Failed to find implementation for interface method: " + ifaceMethod, e);
+                }
+            }
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Method implMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+            Method implMethod = implByInterface.get(method);
+            boolean log = methodsToLog.contains(method);
 
-            if (implMethod.isAnnotationPresent(Log.class)) {
+            if (log) {
                 String params = "";
                 if (args != null) {
                     params = String.join(
                             ", ", Arrays.stream(args).map(String::valueOf).toArray(String[]::new));
                 }
+
                 if (params.isEmpty()) {
                     System.out.println("executed method: " + method.getName());
                 } else {
